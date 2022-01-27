a09f49a3a5c6c2ade4866eed87eacde2b8ea2624
arex123 <ad47kumar@gmail.com>
2022-01-27 16:34:51 +0530

    max product subarray

--- 27jan2022/maxProductofsubarray.java
+++ 27jan2022/maxProductofsubarray.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 class maxProductofsubarray{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+        System.out.println("max product: "+maxProduct(arr));
+    }
+    public static int maxProduct(int[] nums){
+        int max=nums[0];
+        int cmax=nums[0];
+        int cmin=nums[0];
+        int size = nums.length;
+        for(int i=1;i<size;i++){
+            int n = nums[i];
+            int tmp=cmax*n;
+            cmax=Math.max(tmp,Math.max(cmin*n,n));
+            cmin=Math.min(tmp,Math.min(cmin*n,n));
+            
+            max=Math.max(max,cmax);
+            
+        }
+        return max;
+    }
+    public static int maxProductBruteForce(int[] nums) {
+        int max=nums[0];
+        int size = nums.length;
+        for(int i=0;i<size;i++){
+            int tmax=1;
+            for(int j=i;j<size;j++){
+                tmax*=nums[j];
+                max=Math.max(tmax,max);            
+            }
+            
+        }
+        return max;
     }
 }
