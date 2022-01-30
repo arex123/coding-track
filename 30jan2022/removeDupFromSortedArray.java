149dc24eeb3e9f4cc4c77805f38ce15bd6eb309c
arex123 <ad47kumar@gmail.com>
2022-01-30 15:29:55 +0530

    optimized remove duplicate

--- 30jan2022/removeDupFromSortedArray.java
+++ 30jan2022/removeDupFromSortedArray.java
@@ -1,26 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = 1;
+        //optimized code
+        int l=1;
         int n = nums.length;
-        // for(int i=1;i<n;i++){
-        //     if(nums[i]!=nums[i-1]){
-        //         k++;
-        //     }
-        // }
-        
-        int i=0,j=1;
-        while(j<n){
-            if(nums[i]==nums[j]){
-                j++;
-            }else{
-                nums[i+1] = nums[j];
-                j++;
-                i++;
-                k++;
+        for(int r=1;r<n;r++){
+            if(nums[r]!=nums[r-1]){
+                nums[l]=nums[r];
+                l++;
             }
-            
         }
-        
-        return k;
+        return l;
     }
 }
