58866970c8151e3ce5eb56e82aef437de025a758
arex123 <ad47kumar@gmail.com>
2022-02-02 00:33:53 +0530

    max consecutive ones

---
+++ february/02feb2022/maxConsecOnes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int n = nums.length;
+        int tmax = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                // if(i>=1 && nums[i-1]==1){
+                //     tmax++;
+                // }
+                tmax++;
+                max=Math.max(tmax,max);
+            }else{
+                tmax=0;
+            }
+            
+        }
+        
+        return max;
+        
+    }
+}
