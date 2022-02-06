356de42f9062f6dbba4939e744d94362bebd568d
arex123 <ad47kumar@gmail.com>
2022-02-06 18:30:44 +0530

    1312. Minimum Insertion Steps to Make a String Palindrome

---
+++ february/06feb2022/minInsertionToMakeStrPalindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n =s.length();
+        int[][] dp = new int[n+1][n+1];
+        
+        String rev = "";
+        for(int c=n-1;c>=0;c--){
+            rev+=s.charAt(c);
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==rev.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+                
+            }
+        }
+        
+        return n-dp[n][n];
+        
+    }
+}
