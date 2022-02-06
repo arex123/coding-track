636123ffaafba5be9a3592d0231ea6184c4e8b66
arex123 <ad47kumar@gmail.com>
2022-02-06 17:47:22 +0530

    1653. Minimum Deletions to Make String Balanced

---
+++ february/06feb2022/minDelToMakeStringBal.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    //take it as an algorithm
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        int pre = 0;
+        int del = 0;
+        
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='a' && pre>0){
+                pre--;
+                del++;
+            }else if(s.charAt(i)=='b'){
+                pre++;
+            }
+            
+            
+        }
+        
+        return del;
+        
+    }
+}
