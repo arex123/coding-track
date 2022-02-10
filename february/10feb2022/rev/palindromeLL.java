878a216c54c9fc27782f9afea4fb8ce6d8514ce2
arex123 <ad47kumar@gmail.com>
2022-02-10 16:38:56 +0530

    LL palindrome with constant space  revision

--- february/10feb2022/rev/palindromeLL.java
+++ february/10feb2022/rev/palindromeLL.java
@@ -24,3 +24,68 @@ class Solution {
         return true;
     }
 }
+
+/*********************************************/
+// space O(1)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        
+        ListNode slow = head , fast = head;
+        
+        //placing slow to middle of LL
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        //reverse right half of LL
+        slow.next = reverseList(slow.next);
+        
+        //move slow to one step further
+        slow = slow.next;
+        
+        //now make dummynode from start of LL ,
+        // and slow is at new middle(which is reversed),
+        // so dummynode and slow slow should be equal if not then it is not palindrome
+        
+        ListNode dummy = head;
+        while(slow!=null){
+            if(slow.val!=dummy.val){
+                return false;
+            }
+            slow=slow.next;
+            dummy=dummy.next;
+        }
+        
+        return true;
+        
+         
+        
+    }
+    public ListNode reverseList(ListNode head){
+        
+        
+        ListNode newHead = null;
+        while(head!=null){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+            
+        }
+        return newHead;
+        
+        
+    }
+}
