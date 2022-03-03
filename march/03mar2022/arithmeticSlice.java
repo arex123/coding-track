dd2ac3c90b2564b817029d23492b72df2c62c612
arex123 <ad47kumar@gmail.com>
2022-03-03 14:30:45 +0530

    413 Arithmetic Slice

---
+++ march/03mar2022/arithmeticSlice.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/arithmetic-slices/
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        
+        int ans = 0;
+        int[] dp = new int[nums.length];
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i]-nums[i-1]==nums[i+1]-nums[i]){
+                dp[i]=dp[i-1]+1;
+                ans+=dp[i];
+            }
+        }
+        
+        return ans;
+    }
+}
