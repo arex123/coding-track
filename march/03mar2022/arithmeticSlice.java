6abf2dc91eba3f0a4fc29099e190d48db54003b8
arex123 <ad47kumar@gmail.com>
2022-03-04 23:21:50 +0530

    added

--- march/03mar2022/arithmeticSlice.java
+++ march/03mar2022/arithmeticSlice.java
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/arithmetic-slices/
+// https://leetcode.com/problems/arithmetic-slices
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         
