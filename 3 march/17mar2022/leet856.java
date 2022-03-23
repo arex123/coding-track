bd22a3fce44cfcafc7d8ef22393d57f2b0759ba2
arex123 <ad47kumar@gmail.com>
2022-03-23 17:03:18 +0530

    856. Score of Parentheses

---
+++ 3 march/17mar2022/leet856.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/score-of-parentheses/
+class Solution {
+    public int scoreOfParentheses(String S) {
+       Stack<Integer> stack = new Stack<>();
+       int cur = 0;
+       for (char c : S.toCharArray()) {
+           if (c == '(') {
+               stack.push(cur);
+               cur = 0;
+           } else {
+               cur = stack.pop() + Math.max(cur * 2, 1);
+           }
+       }
+       return cur;
+   }
+}
